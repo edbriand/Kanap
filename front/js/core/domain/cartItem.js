b0feefe2ce2f3ec399a1b6edd1078a51ecd7a9fc
Eddy Briand <333ebr@gmail.com>
2022-11-08 11:08:44 +0100

    fix: no negative quantities allowed

--- front/js/core/domain/cartItem.js
+++ front/js/core/domain/cartItem.js
@@ -18,17 +18,29 @@ export class CartItem {
     // Valide l'item: la quantité doit être entre 0 et 100
     // la couleur choisie doit faire partie des couleurs du produit
     validateItem({ product, color, quantity }) {
+        this.validateProduct(product);
+        this.validateQuantity(quantity);
+        this.validateColor(color);
+    }
+
+    validateProduct(product) {
         if (!product) {
             throw new Error("No product found");
         }
+    }
+
+    validateQuantity(quantity) {
         // Verify quantity: if 0 or if > 100, throw error
         if (typeof quantity != "number") {
             throw new Error("Quantity must be a number");
         } else if (quantity > 100) {
             throw new Error("The maximum quantity is 100");
-        } else if (quantity === 0) {
-            throw new Error("Quantity cannot be 0");
+        } else if (quantity <= 0) {
+            throw new Error("Quantity cannot be 0 or inferior");
         }
+    }
+
+    validateColor(color) {
         // Verify color: if unknown for this product, throw error
         if (!product.colors.includes(color)) {
             throw new Error("The color is not a valid color for this product");
